0fccd1ad1a8e180a84fc3eb3d4e1eac49bc4d6e0
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-10-23 21:12:11 +0700

    Update cc.lua

--- cc.lua
+++ cc.lua
@@ -1,4 +1,4 @@
--- VERSION: 2.1
+-- VERSION: 2.2
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
@@ -27,7 +27,7 @@ if not getgenv().PET_TRACKER_RUNNING then
 end
 
 -- Set current version from script
-local currentVersion = "2.1"
+local currentVersion = "2.2"
 getgenv().PET_TRACKER_VERSION = currentVersion
 
 -- Stop any existing instance
@@ -98,7 +98,12 @@ local allowedPets = {
     "Los Jobcitos",
     "Los Combinasionas",
     "Los 67",
-    "Horegini Boom"
+    "Horegini Boom",
+    "Torrtuginni Dragonfrutini"
+    "Pot Hotspot",
+    "Esok Sekolah",
+    "Spaghetti Tualetti",
+    "La Secret Combinasion"
 }
 
 local allowedPetSet = {}

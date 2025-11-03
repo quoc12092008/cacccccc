3572ff42f014c81f3ba6b940118e72d304d7eb1a
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-11-03 22:35:05 +0700

    Update cc.lua

--- cc.lua
+++ cc.lua
@@ -1,4 +1,4 @@
--- VERSION: 2.9
+-- VERSION: 3.0
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
@@ -27,7 +27,7 @@ if not getgenv().PET_TRACKER_RUNNING then
 end
 
 -- Set current version from script
-local currentVersion = "2.9"
+local currentVersion = "3.0"
 getgenv().PET_TRACKER_VERSION = currentVersion
 
 -- Stop any existing instance
@@ -214,8 +214,6 @@ local lastLuckyBlockCheckTime = 0
 
 -- Lucky Block Configuration
 local LUCKY_BLOCKS = {
-    "Spooky Lucky Block",
-    "Admin Lucky Block"
 }
 
 -- Initialize Synchronizer
